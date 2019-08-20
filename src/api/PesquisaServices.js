38e5d9e1fcdb54360c3dd9d55a27a371d6ca267b
TheDataResearch <drocha21@gmail.com>
2019-08-20 01:35:28 -0300

    novo endpoint serviço

--- src/api/PesquisaServices.js
+++ src/api/PesquisaServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:7000/api/',
+  baseURL: 'https://us-central1-enlighten-services.cloudfunctions.net/scrapyServices/api/',
   json: true
 })
 
